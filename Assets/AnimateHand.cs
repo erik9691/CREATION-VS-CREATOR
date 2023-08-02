75058e980c6d4248a79a533898822e572267d966
Erik <111155901+erik9691@users.noreply.github.com>
2023-08-02 16:10:15 -0300

    temp

---
+++ Assets/AnimateHand.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHand : MonoBehaviour
+{
+    public InputActionReference gripReference;
+    public InputActionReference triggerReference;
+
+    Animator _handAnimator;
+    float _gripValue;
+    float _triggerValue;
+
+    void Start()
+    {
+        _handAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        AnimateGrip();
+        AnimateTrigger();
+    }
+
+    void AnimateGrip()
+    {
+        _gripValue = gripReference.action.ReadValue<float>();
+        _handAnimator.SetFloat("Grip", _gripValue);
+    }
+
+    void AnimateTrigger()
+    {
+        _triggerValue = triggerReference.action.ReadValue<float>();
+        _handAnimator.SetFloat("Trigger", _triggerValue);
+    }
+}
