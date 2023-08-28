207d31f329a08daab8306bc063266786304997df
HoFranco2007 <127147452+HoFranco2007@users.noreply.github.com>
2023-08-28 08:57:57 -0300

    spawn ammoBoxes
    Arregle un poco el scitp

--- Assets/Scripts/M&K/SpawnAmmo.cs
+++ Assets/Scripts/M&K/SpawnAmmo.cs
@@ -6,10 +6,18 @@ using Unity.Netcode;
 public class SpawnAmmo : NetworkBehaviour
 {
 
-    [SerializeField] 
-    Transform[] ammoBoxPosition;
+    [SerializeField] Transform[] ammoBoxPosition;
 
     [SerializeField] GameObject ammoBox;
+
+
+    private void Start()
+    {
+        for(int i = 0; i< ammoBoxPosition.Length; i++)
+        {
+            ammoBoxPosition[i].position += new Vector3(0, 1, 0);
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -25,9 +33,10 @@ public class SpawnAmmo : NetworkBehaviour
     [ServerRpc]
     private void SpawnBoxServerRpc()
     {
+        int i = Random.Range(0, ammoBoxPosition.Length);
         GameObject box;
 
-        box = Instantiate(ammoBox, ammoBoxPosition[Random.Range(0, 11)].position, Quaternion.identity);
+        box = Instantiate(ammoBox, ammoBoxPosition[i].position, Quaternion.identity);
         box.GetComponent<NetworkObject>().Spawn();
     }
 }
