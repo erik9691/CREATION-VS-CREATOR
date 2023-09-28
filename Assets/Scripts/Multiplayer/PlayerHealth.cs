e18e10cd9778272045b5f77caf152f4783c1ae78
HoFranco2007 <127147452+HoFranco2007@users.noreply.github.com>
2023-09-28 15:55:34 -0300

    ragdoll algo

--- Assets/Scripts/Multiplayer/PlayerHealth.cs
+++ Assets/Scripts/Multiplayer/PlayerHealth.cs
@@ -7,10 +7,34 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] float playerHealth = 10;
     [SerializeField] float dmgAmount = 1f;
     [SerializeField] float dmgRate = 0.5f;
+    Rigidbody rb;
+    [SerializeField] private Animator animator;
 
     public IEnumerator TakeDamage()
     {
         yield return new WaitForSeconds(dmgRate);
         playerHealth -= dmgAmount;
+    }
+
+    private void Start()
+    {
+        rb = transform.GetComponentInChildren<Rigidbody>();
+    }
+    private void Update()
+    {
+        if(playerHealth <= 0)
+        {
+
+        }
+    }
+
+    void SetEnable(bool enable)
+    {
+        bool isKinematic = !enable;
+        foreach(Rigidbody rigidbody in rb)
+        {
+            rigidbody.isKinematic = isKinematic;
+        }
+        animator.enable = !enable;
     }
 }
