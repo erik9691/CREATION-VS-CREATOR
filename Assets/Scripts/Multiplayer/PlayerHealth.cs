c0a3dd1b5a590efdd51a2792823fab3cf8b5bb78
erik9691 <gagwaffle@gmail.com>
2023-10-03 19:20:22 -0300

    Finished ragdoll and added animations

--- Assets/Scripts/Multiplayer/PlayerHealth.cs
+++ Assets/Scripts/Multiplayer/PlayerHealth.cs
@@ -1,21 +1,123 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlayerHealth : MonoBehaviour
+public class PlayerHealth : NetworkBehaviour
 {
     
     [SerializeField] float playerHealth = 10;
     [SerializeField] float dmgAmount = 1f;
     [SerializeField] float dmgRate = 0.5f;
-    Rigidbody[] rb;
-    [SerializeField] private Animator animator;
+    Rigidbody[] ragdollRb;
+    Collider[] ragdollCol;
+    Rigidbody mainRb;
+    Collider mainCol;
+    [SerializeField] Animator animator;
 
     public IEnumerator TakeDamage()
     {
         yield return new WaitForSeconds(dmgRate);
         playerHealth -= dmgAmount;
     }
+
+    public override void OnNetworkSpawn()
+    {
+        mainCol = GetComponent<Collider>();
+        mainRb = GetComponent<Rigidbody>();
+
+        ragdollRb = GetComponentsInChildren<Rigidbody>(true);
+        ragdollCol = GetComponentsInChildren<Collider>(true);
+
+        //StopRagdollServerRpc();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Ragdoll");
+            StartRagdollServerRpc();
+        }
+    }
+
+    [ServerRpc]
+    void StartRagdollServerRpc()
+    {
+        animator.enabled = false;
+
+        foreach (Collider col in ragdollCol)
+        {
+            col.enabled = true;
+        }
+        foreach (Rigidbody rigid in ragdollRb)
+        {
+            rigid.isKinematic = false;
+        }
+
+        mainCol.enabled = false;
+        mainRb.isKinematic = true;
+
+        StartRagdollClientRpc();
+    }
+
+
+    [ClientRpc]
+    void StartRagdollClientRpc()
+    {
+        animator.enabled = false;
+
+        foreach (Collider col in ragdollCol)
+        {
+            col.enabled = true;
+        }
+        foreach (Rigidbody rigid in ragdollRb)
+        {
+            rigid.isKinematic = false;
+        }
+
+        mainCol.enabled = false;
+        mainRb.isKinematic = true;
+    }
+
+    [ServerRpc]
+    void StopRagdollServerRpc()
+    {
+        animator.enabled = true;
+
+        foreach (Collider col in ragdollCol)
+        {
+            col.enabled = false;
+        }
+        foreach (Rigidbody rigid in ragdollRb)
+        {
+            rigid.isKinematic = true;
+        }
+
+        mainCol.enabled = true;
+        mainRb.isKinematic = false;
+    }
+
+    [ClientRpc]
+    void StopRagdollClientRpc()
+    {
+        animator.enabled = true;
+
+        foreach (Collider col in ragdollCol)
+        {
+            col.enabled = false;
+        }
+        foreach (Rigidbody rigid in ragdollRb)
+        {
+            rigid.isKinematic = true;
+        }
+
+        mainCol.enabled = true;
+        mainRb.isKinematic = false;
+    }
+
+
+
     /*
     private void Start()
     {
