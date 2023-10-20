e41e1dcdad48cc9cc10c78083460343ada17e027
erik9691 <gagwaffle@gmail.com>
2023-10-20 14:39:24 -0300

    Revert "a"
    This reverts commit 295744cd0f6bb919bd2e262274ee44e64031a857.

--- Assets/Scripts/M&K/PlayerMovement.cs
+++ Assets/Scripts/M&K/PlayerMovement.cs
@@ -34,8 +34,6 @@ public class PlayerMovement : NetworkBehaviour
         walkSpeed = _speed;
         cameraTransform = GetComponent<AssignCamera>().cameras.transform.GetChild(0);
 
-        UIManager.Instance.ActivateMinionUI();
-
         //lockear el cursor
         Cursor.lockState = CursorLockMode.Locked;
     }
